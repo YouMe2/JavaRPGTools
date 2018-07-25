8da8bb3487f29cd2e8ea432194422f695958c2e0
YaAlex <yaalex.devlab@gmail.com>
2018-07-25 18:13:16 +0200

    csv and scanner

--- JavaRPGTool/src/HelloRPGTool.java
+++ JavaRPGTool/src/HelloRPGTool.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class HelloRPGTool {
 
 	public static void main(String[] args) {
-		System.out.println("Hello RngEnc");
+		System.out.println("Hello RPG");
+		Scanner sc = new Scanner(System.in);
+
+	    while(sc.hasNext()) {
+	      System.out.println("Damn yo! "+sc.nextLine());
+	    }
 
 	}
 
---
+++ JavaRPGTool/src/tools/RPGTool.java
@@ -0,0 +1,21 @@
+package tools;
+
+import java.util.Scanner;
+
+public class RPGTool {
+
+	public RPGTool() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		Scanner sc = new Scanner(System.in);
+
+	    while(sc.hasNext()) {
+	      System.out.println("Damn "+sc.nextLine());
+	    }
+
+	}
+
+}
