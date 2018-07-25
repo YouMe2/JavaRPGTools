0160d4debf456b6c424ec550691e34948eeb48ce
YaAlex <yaalex.devlab@gmail.com>
2018-07-25 18:51:53 +0200

    basic command structure

--- JavaRPGTool/src/tools/RPGTools.java
+++ JavaRPGTool/src/tools/RPGTools.java
@@ -3,32 +3,53 @@ package tools;
 import java.util.Scanner;
 
 public class RPGTools {
-	
+
 	static final RPGTools instance = new RPGTools();
-	
+
 	static final String WELLCOMEMSG = "Wellcome to RPGTools" + System.lineSeparator() + "- by u/YaAlex";
 	static final String LINEOPENER = "> ";
-	
-	public static void main(String[] args) {	
+
+	public static void main(String[] args) {
 		System.out.println(WELLCOMEMSG);
 		Scanner sc = new Scanner(System.in);
 
-
 		System.out.print(LINEOPENER);
-		while(sc.hasNext()){
+		while (sc.hasNextLine()) {
 			RPGTools.getInstance().doRPGCommand(sc.nextLine());
 		}
 	}
 
 	private RPGTools() {
 	}
-	
+
 	public static RPGTools getInstance() {
 		return instance;
 	}
-	
+
 	public void doRPGCommand(String input) {
-		System.out.println(input + " wooow!");
+		// System.out.println(input + " wooow!");
+
+		String[] in = input.split(" ", 2);
+		String command = in[0];
+		String options = in.length>1 ? in[1] : "";
+
+		switch (command) {
+		case "roll":
+			System.out.println("rolling " + options);
+			break;
+
+		case "exit":
+		case "quit":
+		case "e":
+		case "q":
+			System.out.println("Goodbye!");
+			System.exit(0);
+			break;
+
+		default:
+			System.out.println("No command found: \"" + input + "\"");
+			break;
+		}
 	}
 
 }
