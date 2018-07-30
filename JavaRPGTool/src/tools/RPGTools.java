0ae48a1bdd78b595a4a795df6d4b527af8772d12
YaAlex <yaalex.devlab@gmail.com>
2018-07-30 18:12:14 +0200

    new command structure fully implemented

--- JavaRPGTool/src/tools/RPGTools.java
+++ JavaRPGTool/src/tools/RPGTools.java
@@ -1,6 +1,5 @@
 package tools;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -19,8 +18,8 @@ public class RPGTools {
 	static final RPGTools instance = new RPGTools();
 
 	static final String NEWLINE = System.lineSeparator();
-	static final String WELCOMEMSG = "Welcome to RPGTools" + NEWLINE + "- by u/YaAlex";
-	static final String LINEOPENER = "> ";
+	static final String WELCOMEMSG = "Welcome to RPGTools!" + NEWLINE + "- by u/YaAlex" + NEWLINE + "Try \"?\" for help.";
+	static final String LINEOPENER = ": ";
 
 	public static void main(String[] args) {
 		System.out.println(WELCOMEMSG);
@@ -95,11 +94,11 @@ public class RPGTools {
 		};
 		quitCmd.addTo(commands);
 		
-		rollCmd = new ToolCommand("roll", "r", "[roll, list, table]", "Rolls the specified roll, list of rolls, or table." + NEWLINE
-				+ "Examples: \"roll d20 +3\" or \"roll 6[4d6 dl1]\" or \"6d20 dh2 dl2 ! +5\" or \"roll tableName\" where tableName is the name of an added table" + NEWLINE
-				+ "Roll syntax: [amount]d[die] optional: dh[amount] dl[amount] ! +/-[modifier]" + NEWLINE
-				+ "(\"dh\": drop highest, \"dl\": drop lowest, \"!\": use exploding die)" + NEWLINE
-				+ "List syntax: [amount][[roll]] or [[roll], ... ,[roll]]") {
+		rollCmd = new ToolCommand("roll", "r", "[roll, list, table]", "Rolls the specified roll, list of rolls, or on a table." + System.lineSeparator()
+				+ "\tExamples: \"roll d20 +3\" or \"roll 6[4d6 dl1]\" or \"6d20 dh2 dl2 ! +5\" or \"roll tableName\" where tableName is the name of an added table" + System.lineSeparator()
+				+ "\tRoll syntax: [amount]d[die] optional: dh[amount] dl[amount] ! +/-[modifier]" + System.lineSeparator()
+				+ "\t(\"dh\": drop highest, \"dl\": drop lowest, \"!\": use exploding die)" + System.lineSeparator()
+				+ "\tList syntax: [amount][[roll]] or [[roll], ... ,[roll]]") {
 			
 			@Override
 			public void action(String option) {
@@ -171,8 +170,8 @@ public class RPGTools {
 			
 			@Override
 			public void action(String option) {
-				for (ToolCommand cmd : commands.values()) {
-					System.out.println(cmd.getInfoText());
+				for (Object cmd : commands.values().stream().distinct().toArray()) {
+					System.out.println(((ToolCommand)cmd).getInfoText());
 				}
 				
 			}
@@ -189,88 +188,18 @@ public class RPGTools {
 
 		String[] in = input.split(" ", 2);
 		String command = in[0];
-		String options = in.length > 1 ? in[1] : "";
+		String option = in.length > 1 ? in[1] : "";
 
-		switch (command) {
-		case "path":
-		case "p":
-			path(options);
-			break;
-			
-		case "roll":
-		case "r":
-			roll(options);
-			break;
-
-		case "add":
-		case "a":
-			addTable(options);
-			break;
-
-		case "show":
-		case "s":
-			show(options);
-			break;
-			
-		case "help":
-		case "h":
-			help(options);
-			break;
-
-		case "exit":
-		case "quit":
-		case "e":
-		case "q":
-			exit(options);
-
-			break;
-
-		default:
+		if (!commands.containsKey(command))
 			System.out.println("No command found: \"" + input + "\"");
-			break;
+		else {
+			
+			ToolCommand cmd = commands.get(command);
+			cmd.action(option);
 		}
-	}
-
-	private void show(String input) {
-		RollableTable t = tables.get(input);
-		if (t != null) {
-			System.out.println(t.toString());
-		} else
-			System.out.println("No table found: \"" + input + "\"");
-	}
-
-	private void exit(String options) {
-		System.out.println("Goodbye!");
-		System.exit(0);
-	}
-
-	private void help(String options) {
+			
 		
-		System.out.println("Try one of the following commands:" + NEWLINE);
 		
-		System.out.println("try \"roll 6[4d6dl1]\" or \"r d20\"");
-		System.out.println("or try \"add [table].cvs\"");
-
-	}
-	
-	private void path(String options) {
-		System.out.println("user.dir = " + System.getProperty("user.dir"));
-	}
-
-	public void addTable(String input) {
-		String filecontent;
-		try {
-			filecontent = readFile(input, Charset.defaultCharset());
-			RollableTable t = RollableTable.tryParse(filecontent);
-			tables.put(t.getName(), t);
-			System.out.println("Added Table " + t.getName());
-
-		} catch (IOException e) {
-			System.out.println("Couldn't read the file. Try again.");
-		} catch (ParseException e) {
-			System.out.println("Couldn't parse the table. Please correct it and try again.");
-		}
-
 	}
 
 	static String readFile(String path, Charset encoding) throws IOException {
@@ -278,25 +207,5 @@ public class RPGTools {
 		return new String(encoded, encoding);
 	}
 
-	public void roll(String input) {
-		try {
-			DiceRoll dice = DiceRoll.tryParse(input);
-			System.out.println("Rolling " + dice + ": " + dice.roll());
-		} catch (Exception e) {
-			try {
-				ListRoll list = ListRoll.tryParse(input);
-				System.out.println("Rolling " + list + ": " + Arrays.toString(list.roll()));
-			} catch (Exception e2) {
-				RollableTable t = tables.get(input);
-				if (t != null) {
-					System.out.println("Rolling on Table " + t.getName() + ":");
-					int res = t.getTableroll().roll();
-					System.out.println(res+" -> " + t.getEntry(res));
-				} else
-					System.out.println("No valid roll found: \"" + input + "\"");
-			}
-		}
-
-	}
 
 }
--- JavaRPGTool/src/tools/ToolCommand.java
+++ JavaRPGTool/src/tools/ToolCommand.java
@@ -8,7 +8,7 @@ public abstract class ToolCommand {
 	private final String commandKey_alt;
 	private final String options;
 	private final String description;
-	
+
 	public ToolCommand(String commandKey_full, String commandKey_alt, String options, String desc) {
 		this.commandKey_full = commandKey_full;
 		this.commandKey_alt = commandKey_alt;
@@ -17,30 +17,32 @@ public abstract class ToolCommand {
 	}
 
 	public String getInfoText() {
-		return getCommandKey_full()+", "+getCommandKey_alt()+" "+getOptions()+System.lineSeparator()+getDescription();
+		return getCommandKey_full() + ", " + getCommandKey_alt() + System.lineSeparator() 
+				+ " args: " + getOptions() + System.lineSeparator() 
+				+ "\t" + getDescription();
 	}
-	
+
 	public String getOptions() {
 		return options;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public String getCommandKey_full() {
 		return commandKey_full;
 	}
-	
+
 	public String getCommandKey_alt() {
 		return commandKey_alt;
 	}
-	
+
 	public void addTo(Map<String, ToolCommand> map) {
 		map.put(getCommandKey_full(), this);
 		map.put(getCommandKey_alt(), this);
 	}
-	
+
 	public abstract void action(String option);
-	
+
 }
